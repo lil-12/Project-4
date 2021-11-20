1eebec2956927976dde42e0abe8897c397e7813f
lil-12 <90734020+lil-12@users.noreply.github.com>
2021-11-19 21:52:03 -0500

    Add files via upload

--- CustomerChat.java
+++ CustomerChat.java
@@ -1,7 +1,6 @@
-import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class CustomerChat extends Application {
+public class CustomerChat {
 
 	public CustomerChat() {
 		// TODO Auto-generated constructor stub
